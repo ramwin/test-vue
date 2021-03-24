f30acf82e1e00f68d7fc38001bd4c3d22e89c906
Xiang Wang <ramwin@qq.com>
2021-03-24 18:49:58 +0800

    测试vxe-table

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
   <div id="app">
+    <h1>测试vxe-grid</h1>
+    <vxe-grid
+      v-bind="gridOptions"
+    >
+      <template v-slot:title>
+        <span>我是自定义的title</span>
+      </template>
+      <template v-slot:customersearch>
+        <span>我是自定义的搜索</span>
+      </template>
+    </vxe-grid>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -12,6 +23,31 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      gridOptions: {
+        formConfig: {
+          items: [
+            {
+              field: "search",
+              title: "默认的搜索",
+              itemRender: {
+                name: "$input",
+              },
+            },
+            {
+              field: "customersearch",
+              title: "自定义的搜索",
+              slots: {
+                title: "title",
+                default: "customersearch",
+              },
+            },
+          ]
+        },
+      },
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'vxe-utils'
+import VXETable from "vxe-table"
+import "vxe-table/lib/style.css"
 
 Vue.config.productionTip = false
+Vue.use(VXETable)
 
 new Vue({
   render: h => h(App),
